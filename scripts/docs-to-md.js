4fbcb670d15ca77bda6a11073e4b01d2b1e2d8bf
sanabel-al-firdaws <136203274+sanabel-al-firdaws@users.noreply.github.com>
2024-09-18 15:14:56 +0300

    Update docs-to-md.js

--- scripts/docs-to-md.js
+++ scripts/docs-to-md.js
@@ -17,6 +17,7 @@ const arabic = [
    '17jzutUJX4CqVkREE2eOsd0DBrPiTUmcczbHam0cQA0U:al-sunnah-imam-ahmed.mdx'
  ];
  const blog = [
+   '1XbhdCLMoBMV5FKMwZIOl4x5uOEHeCt8P0hDcvODt7jQ:sabegh',
    '1RsN0pOVPzjWbZaCQYWZKwLA3znQ6S9belC8Fu1Gn2yc:al-mawlid.mdx',
    '15azTT_OD1Tl1Gth-IrujKp2ILdcPkAXdTtPZZ0AsQsE:al-inkar-al-alani.mdx',
    '1rmsrXsVQLce98QUGagKuL3mterFxPV1QHZzpPU5yUuM:al-hakim-al-motaghalib.mdx',
